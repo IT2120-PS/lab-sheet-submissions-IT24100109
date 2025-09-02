1276e04c408eb63bdb4eccf7164fd984ef930fe1
IT24100109 <it24100109@my.sliit.lk>
2025-09-02 14:27:08 +0530

    Add IT24100109_Lab6.R

---
+++ Lab Sheet 06/IT24100109_Lab6.R
@@ -0,0 +1,26 @@
+getwd()
+setwd("C:\\Users\\IT24100109\\Desktop\\IT24100109")
+getwd()
+
+# question 01
+# part 1
+#Binomial Distribution
+#Here, random variable X has binomial distribution with n=50 and p=0.85
+
+# question 01
+# part 2
+dbinom(47,50,0.85)
+
+
+# question 02
+# part 1
+# number of calls receives in a call center on a given hour
+
+# question 02
+# part 2
+# poisson distribution
+# here, random variable X has poisson distribution with lamda=12
+
+# question 02
+# part 3
+dpois(15,12)
